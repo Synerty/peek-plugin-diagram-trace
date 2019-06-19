54eeae0f5869ea50f69e5cd9f9b2a6f7d32b0adf
Jarrod Chesney <jarrod.chesney@synerty.com>
2019-06-19 14:53:24 +1000

    Updated package publish scripts.
    PEEK-321

--- peek_plugin_diagram_trace/__init__.py
+++ peek_plugin_diagram_trace/__init__.py
@@ -3,7 +3,7 @@ from peek_plugin_base.server.PluginServerEntryHookABC import PluginServerEntryHo
 from peek_plugin_base.client.PluginClientEntryHookABC import PluginClientEntryHookABC
 from typing import Type
 
-__version__ = '1.3.2'
+__version__ = '0.0.0'
 
 
 def peekServerEntryHook() -> Type[PluginServerEntryHookABC]:
---
+++ publish.settings.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+PY_PACKAGE="peek_plugin_diagram_trace"
+PYPI_PUBLISH="1"
+
+VER_FILES=""
